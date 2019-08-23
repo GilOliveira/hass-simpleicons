8119c5181ebfb41fa0db0124844f1502641701e6
Danny <github@dne.name>
2019-08-23 15:44:30 +0200

    Update and convert fork to simple-icons 1.13.0

--- generate.py
+++ generate.py
@@ -11,8 +11,9 @@ def make_file(directory, name, out):
             name = os.path.splitext(f)[0]
             data = fp.read()
             size_start = data.find('viewBox="')+len('viewBox="')
-            size_end = data[size_start:].find('">')
+            size_end = data[size_start:].find('"')
             size = data[size_start:size_start+size_end].split(' ')
+            print (size)
             width = float(size[2])
             height = float(size[3])
             scale = max(width, height)/ICON_SIZE
@@ -20,11 +21,7 @@ def make_file(directory, name, out):
             out.write('<g id="{0}" transform="scale({1} {1})">'.format(name, 1/scale) + data[start:-6] + '</g>\n')
     out.write('</defs></svg></ha-iconset-svg>')
 
-# Run this in Font-Awesome/svgs
 
-# with open("hass-fontawesome-brands.html", 'w') as out:
-#     make_file('brands', 'fab', out)
-# with open("hass-fontawesome-regular.html", 'w') as out:
-#     make_file('regular', 'far', out)
-# with open("hass-fontawesome-solid.html", 'w') as out:
-#     make_file('solid', 'fas', out)
+# Run this in /simple-icons
+ with open("hass-simple-icons.html", 'w') as out:
+  make_file('icons', 'si', out)
